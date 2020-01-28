e528787f458cae7a5b961445271e5fd1ccac04c9
Nelly Mahecha <nejumava@gmail.com>
2020-01-28 17:15:06 -0500

    Creacion de modelo y datos para Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Creacion de categorias
+Categories = Category.create([
+    {name: 'Restaurants'},
+    {name: 'Grocery'},
+    {name: 'Car'},
+    {name: 'Services'},
+    {name: 'Home'},
+    {name: 'Education'},
+    {name: 'Fun'},
+    {name: 'Travel'}
+])
