4b1ab3613855404858441d147d8e7c9a6bb286ac
Nelly Mahecha <nejumava@gmail.com>
2020-01-28 12:18:19 -0500

    Routes para API

---
+++ app/controllers/api/v1/expenses_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ExpensesController < ApplicationController
+    def index
+        @expenses = Expense.all
+        render json: @expenses
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
 
   resources :expenses, only: [:index]
 
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :expenses
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
