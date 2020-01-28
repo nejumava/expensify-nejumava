f1cd255854b67033b61f93eb27411f8765eedd40
Nelly Mahecha <nejumava@gmail.com>
2020-01-28 17:32:56 -0500

    Validaciones para Expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,9 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validates :type, presence: true
+  validates :date_expense, presence: true
+  validates :concept, presence: true
+  validates :category, presence: true
+  validates :amount, presence: true
 end
